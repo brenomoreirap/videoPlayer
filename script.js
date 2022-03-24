0a6810e92ab865e26c95316fc2bc47a68d9ec7b4
Breno Moreira <brenomoreirapro@gmail.com>
2022-03-24 15:05:42 -0300

    script.js code refactoring

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-var video = document.querySelector("#video")
-var volumeContainer = document.querySelector(".volume-container")
-var volumeBtn = document.querySelector("#volumeBtn")
-var volumeBar = document.querySelector("#volumeBar")
+let video = document.querySelector("#video")
+let volumeContainer = document.querySelector(".volume-container")
+let volumeBtn = document.querySelector("#volumeBtn")
+let volumeBar = document.querySelector("#volumeBar")
+let container = document.querySelector(".container")
+let controls = document.querySelector(".controls")
 
 onload = function () {
     video.addEventListener("dblclick", fullscreen)
     volumeContainer.addEventListener("mouseover", showVolumeBar)
     volumeBar.addEventListener("change", volumeController)
-    volumeBtn.addEventListener("click", function () {
-
-        if (video.volume != 0) {
-            video.volume = 0
-            volumeBar.value = 0
-            volumeBtn.classList.remove("fa-volume-high")
-            volumeBtn.classList.remove("fa-volume-low")
-            volumeBtn.classList.add("fa-volume-xmark")
-
-        } else if (video.volume == 0) {
-            video.volume = 0.5
-            volumeBar.value = 30
-            volumeBtn.classList.remove("fa-volume-high")
-            volumeBtn.classList.remove("fa-volume-xmark")
-            volumeBtn.classList.add("fa-volume-low")
-        }
-    })
-    document.querySelector(".container").addEventListener("mouseover", function () {
-        document.querySelector(".controls").style.bottom = "0"
-        document.querySelector(".container").addEventListener("mouseleave", function () {
-            document.querySelector(".controls").style.bottom = "-3.125rem"
-        })
-    })
+    container.addEventListener("mouseover", showControls)
 }
 
 function playPause() {
@@ -68,25 +48,32 @@ function speedUp() {
     video.playbackRate += 0.5
 }
 
+function showControls() {
+    controls.style.bottom = "0"
+    container.addEventListener("mouseleave", function () {
+        controls.style.bottom = "-3.125rem"
+    })
+}
+
 function showVolumeBar() {
-    document.querySelector("#volumeBar").style.display = "inline-block"
+    volumeBar.style.display = "inline-block"
     volumeContainer.addEventListener("mouseleave", function () {
-        document.querySelector("#volumeBar").style.display = "none"
+        volumeBar.style.display = "none"
     })
 }
 
 function volumeController() {
-    video.volume = document.querySelector("#volumeBar").value / 100
+    video.volume = volumeBar.value / 100
 
     if (video.volume == 0) {
+        volumeBtn.classList.add("fa-volume-xmark")
         volumeBtn.classList.remove("fa-volume-high")
         volumeBtn.classList.remove("fa-volume-low")
-        volumeBtn.classList.add("fa-volume-xmark")
 
     } else if (video.volume > 0 && video.volume < 0.5) {
+        volumeBtn.classList.add("fa-volume-low")
         volumeBtn.classList.remove("fa-volume-high")
         volumeBtn.classList.remove("fa-volume-xmark")
-        volumeBtn.classList.add("fa-volume-low")
 
     } else {
         volumeBtn.classList.add("fa-volume-high")
